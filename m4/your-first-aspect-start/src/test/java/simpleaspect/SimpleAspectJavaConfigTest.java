5f501104e0128af44d0e8bd1d04b517de32a407c
招文桃 <gdouzwt@gmail.com>
2020-07-03 03:12:16 +0800

    收拾完 m4

--- m4/your-first-aspect-start/src/test/java/simpleaspect/SimpleAspectJavaConfigTest.java
+++ m4/your-first-aspect-start/src/test/java/simpleaspect/SimpleAspectJavaConfigTest.java
@@ -1,16 +1,15 @@
 package simpleaspect;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import configuration.SimpleAspectConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 @ContextConfiguration(classes = SimpleAspectConfiguration.class)
 public class SimpleAspectJavaConfigTest {
 
--- m4/your-first-aspect-start/src/test/java/simpleaspect/SimpleAspectXMLTest.java
+++ m4/your-first-aspect-start/src/test/java/simpleaspect/SimpleAspectXMLTest.java
@@ -1,14 +1,16 @@
 package simpleaspect;
 
-import static org.junit.Assert.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
 @ContextConfiguration("/simpleaspect.xml")
 public class SimpleAspectXMLTest {
 
